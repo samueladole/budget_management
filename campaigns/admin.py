605abfca2157f60ab57a1863a90ad6cc9be2a769
Samuel Adole <adoleichasamuel@gmail.com>
2025-07-02 00:23:53 +0100

    Setup models for campaign app

---
+++ campaigns/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from .models import Brand, Campaign, SpendLog, DaypartingSchedule
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ("name", "daily_budget", "monthly_budget")
+    search_fields = ("name",)
+
+
+@admin.register(Campaign)
+class CampaignAdmin(admin.ModelAdmin):
+    list_display = (
+        "name", "brand", "is_active", "current_daily_spend", "current_monthly_spend"
+    )
+    list_filter = ("is_active", "brand")
+    search_fields = ("name",)
+
+
+@admin.register(SpendLog)
+class SpendLogAdmin(admin.ModelAdmin):
+    list_display = ("campaign", "timestamp", "amount")
+    list_filter = ("campaign", "timestamp")
+    readonly_fields = ("timestamp",)
+
+
+@admin.register(DaypartingSchedule)
+class DaypartingScheduleAdmin(admin.ModelAdmin):
+    list_display = ("campaign", "start_hour", "end_hour")
+    list_filter = ("start_hour", "end_hour")
